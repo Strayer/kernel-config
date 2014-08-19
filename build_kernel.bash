d9f8616129add79d42750c65b0601e085a897598
Sven Grunewaldt <strayer@olle-orks.org>
2014-08-19 16:30:24 +0200

    Auto commit new configs

--- build_kernel.bash
+++ build_kernel.bash
@@ -1,3 +1,18 @@
 #!/bin/bash
-cd ${0%/*}
-genkernel --install --symlink --no-splash --lvm --kernel-config=./config all
+
+script_path="`pwd`"
+kernel_path="/usr/src/linux"
+
+cp "${script_path}/config" "${kernel_path}/.config"
+
+cd "${kernel_path}"
+make oldconfig
+kernel_version=`make kernelversion`
+
+cp "${kernel_path}/.config" "${script_path}/config"
+
+genkernel --install --symlink --no-splash --lvm --kernel-config="${script_path}/config" all
+
+cd "${script_path}"
+git add config
+git commit --verbose -e -m "linux-${kernel_version}"
