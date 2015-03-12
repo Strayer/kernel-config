b82ddc03572d29ae935edc597bf68d4deb4d0160
Sven Grunewaldt <strayer@olle-orks.org>
2015-03-12 12:56:23 +0100

    Separate config preparation and build script

---
+++ build_kernel.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+script_path="`pwd`"
+kernel_path="/usr/src/linux"
+
+cd "${kernel_path}"
+kernel_version=`make kernelversion`
+
+time genkernel --install --symlink --no-splash --lvm --kernel-config="${script_path}/config" all
+
+cd "${script_path}"
+git add config
+git commit --verbose -m "linux-${kernel_version}"
--- prepare_config.sh
+++ prepare_config.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 script_path="`pwd`"
 kernel_path="/usr/src/linux"
@@ -7,12 +7,9 @@ cp "${script_path}/config" "${kernel_path}/.config"
 
 cd "${kernel_path}"
 make oldconfig
-kernel_version=`make kernelversion`
 
 cp "${kernel_path}/.config" "${script_path}/config"
 
-genkernel --install --symlink --no-splash --lvm --kernel-config="${script_path}/config" all
-
 cd "${script_path}"
-git add config
-git commit --verbose -e -m "linux-${kernel_version}"
+
+git diff HEAD "${script_path}/config"
